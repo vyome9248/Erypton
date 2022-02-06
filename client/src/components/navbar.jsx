c511c9758e040d02431a658e73554069464c42d2
vyome9248 <85108695+vyome9248@users.noreply.github.com>
2022-02-07 02:33:10 +0530

    Update navbar.jsx

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
 
             </div>
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                {["Leaderboard", "Exclusive Content", "Connect via private message"].map((item, index) => (
                     <NavbarItem key={item + index} title={item} />
                 ))}
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
