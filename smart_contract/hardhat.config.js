63fd20cc4c22a481d92c2a9c6a1bfcb6b02a55ac
vyome9248 <85108695+vyome9248@users.noreply.github.com>
2022-02-07 05:08:36 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,5 +1,5 @@
 //https://polygon-mumbai.g.alchemy.com/v2/BNMLqVBLGHMTEkJL2v3prkak-Lv-Fk0x
-
+// I have not included the private key
 require('@nomiclabs/hardhat-waffle')
 
 module.exports = {
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     mumbai: {
       url: 'https://polygon-mumbai.g.alchemy.com/v2/BNMLqVBLGHMTEkJL2v3prkak-Lv-Fk0x',
-      accounts: [ 'PRIVATE' ]
+      accounts: [ 'PRIVATE' ] 
     }
   }
 }
