470103d0e9d680fc6a6d20ad134b4adfc2d72448
vyome9248 <85108695+vyome9248@users.noreply.github.com>
2022-02-07 02:57:17 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,13 +10,16 @@ const Footer = () => {
                 </div>
                 <div className='flex justify-center items-center flex-col mt-5'>
                     <p className='text-white text-sm text-center'>Write to us</p>
-                    <p className='text-white text-sm text-center'>info@krateon.com</p>
+                    <p className='text-white text-sm text-center'>info@tippy.com</p>
                 </div>
-                <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5' />
+                <div className="flex-1 flex flex-col justify-start items-start">
+                    <h1 className="text-white text-5xl sm:text-5xl py-2 text-gradient"> TIPPY
+                    </h1>
+             
+                    <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5' />
 
-                <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
-                <p className='text-white text-sm text-center'>@krateon 2022</p>
-                <p className='text-white text-sm text-center'>All rights reserved</p>
+              
+
                 </div>
 
            
